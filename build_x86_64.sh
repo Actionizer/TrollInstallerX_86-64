db22f2084084589f6553639231da1be74df75f22
MineTurtlee <97010924+MineTurtlee@users.noreply.github.com>
2025-08-02 16:10:56 +0700

    More Overrides!

--- build_x86_64.sh
+++ build_x86_64.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-xcodebuild -configuration Release -derivedDataPath DerivedData/TrollInstallerX -destination 'platform=iOS Simulator' -scheme TrollInstallerX CODE_SIGNING_ALLOWED="NO" CODE_SIGNING_REQUIRED="NO" CODE_SIGN_IDENTITY=""
+xcodebuild -configuration Release -derivedDataPath DerivedData/TrollInstallerX -destination 'platform=iOS Simulator' -scheme TrollInstallerX CODE_SIGNING_ALLOWED="NO" CODE_SIGNING_REQUIRED="NO" CODE_SIGN_IDENTITY="" ARCHS=x86_64 VALID_ARCHS=x86_64
 cp Resources/ents.plist DerivedData/TrollInstallerX/Build/Products/Release-iphoneos/
 pushd DerivedData/TrollInstallerX/Build/Products/Release-iphoneos
 rm -rf Payload TrollInstallerX.ipa
